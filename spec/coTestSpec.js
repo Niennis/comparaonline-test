57edcc58671c9c39c22d55f26a0aa02d4c7d7dd0
Estefania Osses Vera <estefania.osses.v@gmail.com>
2018-07-13 21:28:14 -0400

    Tests for Mega Coverage and Super Sale done

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -4,12 +4,25 @@ const coTest = require('../src/coTest');
 const CarInsurance = coTest.CarInsurance;
 const Product = coTest.Product;
 
-describe("Co Test", function() {
+describe("Co Test", function () {
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
+  it("Super Sale drops 2", function () {
+    const coTest = new CarInsurance([new Product("Super Sale", 10, 10)]);
     const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+
+    expect(products[0].name).equal("Super Sale");
+    expect(products[0].sellIn).equal(9);
+    expect(products[0].price).equal(8);
   });
+
+  it("Mega Coverage never changes", function () {
+    const coTest = new CarInsurance([new Product("Mega Coverage", 0, 80)]);
+    const products = coTest.updatePrice();
+    
+  expect(products[0].name).equal('Mega Coverage');
+  expect(products[0].sellIn).equal(0);
+  expect(products[0].price).equal(80);
+  });
+
 
 });
