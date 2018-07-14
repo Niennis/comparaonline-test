6952c1684239323c461f335206e61b58ae6e1bb4
Estefania Osses Vera <estefania.osses.v@gmail.com>
2018-07-13 21:54:40 -0400

    Tests completed

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -6,6 +6,69 @@ const Product = coTest.Product;
 
 describe("Co Test", function () {
 
+  it("Prices never negatives", function () {
+    const coTest = new CarInsurance([new Product("Medium Coverage", 1, 0)]);
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal('Medium Coverage');
+    expect(products[0].sellIn).equal(0);
+    expect(products[0].price).equal(0);
+  });
+
+  it("If sellIn equals 0, then price drops 2", function () {
+    const coTest = new CarInsurance([new Product("Low Coverage", -1, 20)]);
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal('Low Coverage');
+    expect(products[0].sellIn).equal(-2);
+    expect(products[0].price).equal(18);
+  });
+
+  it("Prices never over 50, and Full Coverage always increases", function () {
+    const coTest = new CarInsurance([new Product("Full Coverage", 0, 50)]);
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal('Full Coverage');
+    expect(products[0].sellIn).equal(-1);
+    expect(products[0].price).equal(50);
+  });
+
+  it("Mega Coverage price never changes", function () {
+    const coTest = new CarInsurance([new Product("Mega Coverage", 0, 80)]);
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal('Mega Coverage');
+    expect(products[0].sellIn).equal(0);
+    expect(products[0].price).equal(80);
+  });
+
+  it("Special Full Coverage increases 2 if sellIn < 11", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 10, 20)]);
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal('Special Full Coverage');
+    expect(products[0].sellIn).equal(9);
+    expect(products[0].price).equal(22);
+  });
+
+  it("Special Full Coverage increases 3 if sellIn < 6", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 5, 20)]);
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal('Special Full Coverage');
+    expect(products[0].sellIn).equal(4);
+    expect(products[0].price).equal(23);
+  });
+
+  it("Special Full Coverage price drops to 0 if sellIn < 0", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 0, 20)]);
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal('Special Full Coverage');
+    expect(products[0].sellIn).equal(-1);
+    expect(products[0].price).equal(0);
+  });
+
   it("Super Sale drops 2", function () {
     const coTest = new CarInsurance([new Product("Super Sale", 10, 10)]);
     const products = coTest.updatePrice();
@@ -14,15 +77,5 @@ describe("Co Test", function () {
     expect(products[0].sellIn).equal(9);
     expect(products[0].price).equal(8);
   });
-
-  it("Mega Coverage never changes", function () {
-    const coTest = new CarInsurance([new Product("Mega Coverage", 0, 80)]);
-    const products = coTest.updatePrice();
-    
-  expect(products[0].name).equal('Mega Coverage');
-  expect(products[0].sellIn).equal(0);
-  expect(products[0].price).equal(80);
-  });
-
 
 });
--- src/coTest.js
+++ src/coTest.js
@@ -12,19 +12,19 @@ class CarInsurance {
   }
   updatePrice() {
     for (var i = 0; i < this.products.length; i++) {
-      if (this.products[i].name != 'Full Coverage' && this.products[i].name != 'Special Full Coverage') {
+      if (this.products[i].name !== 'Full Coverage' && this.products[i].name !== 'Special Full Coverage') {
         if (this.products[i].price > 0) {
-          if (this.products[i].name != 'Mega Coverage' && this.products[i].name != 'Super Sale') {
+          if (this.products[i].name !== 'Mega Coverage' && this.products[i].name !== 'Super Sale') {
             this.products[i].price = this.products[i].price - 1;
           }
-          if (this.products[i].name == 'Super Sale') {
+          if (this.products[i].name === 'Super Sale') {
             this.products[i].price = this.products[i].price -2;
           }
         }
       } else {
         if (this.products[i].price < 50) {
           this.products[i].price = this.products[i].price + 1;
-          if (this.products[i].name == 'Special Full Coverage') {
+          if (this.products[i].name === 'Special Full Coverage') {
             if (this.products[i].sellIn < 11) {
               if (this.products[i].price < 50) {
                 this.products[i].price = this.products[i].price + 1;
@@ -32,21 +32,21 @@ class CarInsurance {
             }
             if (this.products[i].sellIn < 6) {
               if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 2;
+                this.products[i].price = this.products[i].price + 1;
               }
             }
           }
         }
       }
-      if (this.products[i].name != 'Mega Coverage') {
+      if (this.products[i].name !== 'Mega Coverage') {
         this.products[i].sellIn = this.products[i].sellIn - 1;
       }
       if (this.products[i].sellIn < 0) {
-        if (this.products[i].name != 'Full Coverage') {
-          if (this.products[i].name != 'Special Full Coverage') {
+        if (this.products[i].name !== 'Full Coverage') {
+          if (this.products[i].name !== 'Special Full Coverage') {
             if (this.products[i].price > 0) {
-              if (this.products[i].name != 'Mega Coverage' && this.products[i].name != 'Super Sale') {
-                this.products[i].price = this.products[i].price - 2;
+              if (this.products[i].name !== 'Mega Coverage' && this.products[i].name !== 'Super Sale') {
+                this.products[i].price = this.products[i].price - 1;
               }
             }
           } else {
